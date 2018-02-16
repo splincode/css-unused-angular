f68a0e84e586daac1f66c281b700552cd64aed72
splincode <omaxphp@yandex.ru>
2018-02-16 23:18:57 +0300

    webpack detect problem generate css hash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [
-  	'./app.component.css',
-  	'./materialize.css'
+    './app.component.css',
+    './materialize.css'
   ]
 })
 export class AppComponent {
