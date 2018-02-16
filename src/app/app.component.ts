c1ef2e8ae2a76ce1c3436244a8f1c3e0b000929c
mivanov <mivanov@cinimex.ru>
2018-02-16 15:00:53 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+  	'./app.component.css',
+  	'./materialize.css'
+  ]
 })
 export class AppComponent {
   title = 'app';
